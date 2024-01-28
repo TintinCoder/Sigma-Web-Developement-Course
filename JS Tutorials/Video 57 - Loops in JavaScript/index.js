2bc15ce832e8d821d14b7b5b135d229e1268ab71
TintinCoder <divyamyadav40@gmail.com>
2024-01-28 19:56:22 +0530

    Completed Video 57 - Completed the loops

---
+++ JS Tutorials/Video 57 - Loops in JavaScript/index.js
@@ -0,0 +1,47 @@
+console.log("Loops in JavaScript")
+
+let a = 1;
+
+// For Loops
+
+// for (let i = 0; i < 101; i++) {
+//     console.log(i)
+// }
+
+// For In loop
+
+// let obj = {
+//     name: "Harry",
+//     role: "Programmer",
+//     company: "Google"
+// }
+
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(element)
+//     }
+// }
+
+// For of loop
+
+// for (const c of "Tinny") {
+//     console.log(c)
+// }
+
+// While loop
+
+// let i = -10;
+// while(i<6){
+//     console.log(i)
+//     i++;
+// }
+
+// Do-While loop
+
+// let i =0;
+
+// do {
+//     console.log(i)
+//     i++;
+// } while (10>5);
