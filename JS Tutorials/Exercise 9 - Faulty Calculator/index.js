bf501dd2398567fa419d90f243b10ab43f800fdd
TintinCoder <Divyamyadav40@gmail.com>
2024-01-28 20:18:37 +0530

    Completed Exercise 9 - Faulty Calculator

--- JS Tutorials/Exercise 9 - Faulty Calculator/index.js
+++ JS Tutorials/Exercise 9 - Faulty Calculator/index.js
@@ -6,3 +6,31 @@
 / ---> **
 
 */
+
+let possibity = (Math.random() * 100).toFixed(0);
+
+const num1 = Number.parseInt(prompt("Enter the first number -: "))
+const num2 = Number.parseInt(prompt("Enter the second number -: "))
+const operator = prompt("Enter the operation you want -: ")
+
+if(possibity <= 10){
+    if(operator == "+"){
+        alert(num1-num2)
+    } else if(operator == "*"){
+        alert(num1+num2)
+    } else if(operator == "-"){
+        alert(num1/num2)
+    } else if(operator == "/"){
+        alert(num1**num2)
+    }
+} else if(possibity > 10) {
+    if(operator == "+"){
+        alert(num1+num2)
+    } else if(operator == "-"){
+        alert(num1-num2)
+    } else if(operator == "*"){
+        alert(num1*num2)
+    } else if(operator == "/"){
+        alert(num1/num2)
+    }
+}
