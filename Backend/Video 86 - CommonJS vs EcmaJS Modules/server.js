b4e047e6ecf8e21d8576b10abf95b7c58717ef88
TintinCoder <Divyamyadav40@gmail.com>
2024-02-23 20:54:19 +0530

    Video 86 - Completed EcmaJS vs Common JS(Importing modules in it require vs import)

--- Backend/Video 86 - CommonJS vs EcmaJS Modules/server.js
+++ Backend/Video 86 - CommonJS vs EcmaJS Modules/server.js
@@ -1,12 +1,5 @@
-const http = require('node:http')
-const hostname = '127.0.0.1'
-const port = 6969
-const server = http.createServer((req, res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain')
-    res.end("Hello World\n")
-})
+// import {a, b, d, e} from './mymodule.js'
+// console.log(a, b, e, d)
 
-server.listen(port, hostname, ()=>{
-    console.log(`Server running at http://${hostname}:${port}/`)
-})
+const a = require("./mymodule2.js")
+console.log(a, __dirname, __filename)
