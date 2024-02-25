732cfa458434f72d84cf476bf6874f699346d861
TintinCoder <Divyamyadav40@gmail.com>
2024-02-25 15:33:49 +0530

    Video 89 - Completed video 89

--- Backend/Video 89 - Response, Request and Routers in Express/server.js
+++ Backend/Video 89 - Response, Request and Routers in Express/server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
+const blogs = require('./routes/blog')
+const port = 3000;
+
+app.use(express.static("public"))
+app.use('/blog', blogs)
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
 
-app.get("/*", (req, res)=>{
-    res.send("Page is not found found not found not found!!!")
+app.get("/index/", (req, res) => {
+    // res.sendFile("templates/index.html", {root: __dirname}) This is how to send a file to an endpoint 
+    res.json({a: 10, b: 20, c: 30, d: 40})
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+app.listen(port, () => {
+    console.log(`Server running on port ${port}. The URL is http://localhost:${port}`);
+})
