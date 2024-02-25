5748738db4967ac435527642197ccbc7c28bf17b
TintinCoder <Divyamyadav40@gmail.com>
2024-02-25 18:44:01 +0530

    Video 90 - Completed Middlewares In Express JS

--- Backend/Video 90 - Middlewares In Express JS/server.js
+++ Backend/Video 90 - Middlewares In Express JS/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
+const fs = require("fs")
 const port = 3000;
+
+// app.use(express.static("public"))
+
+// Middleware 1 - Logger for our application
+app.use((req, res, next)=>{
+    console.log(`${Date.now()} is a ${req.method}`)
+    // res.send('Hacked by middleware 1')
+    next()
+})
+
+// Middleware 2
+app.use((req, res, next)=>{
+    console.log('Middleware 2')
+    next()
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is listening on port port`);
+    console.log(`Server is listening on port ${port}`);
 });
