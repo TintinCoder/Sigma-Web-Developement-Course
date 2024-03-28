a59fadd444301ecc715670e580003f63df5bb6e0
TintinCoder <Divyamyadav40@gmail.com>
2024-03-28 07:45:30 +0530

    Started Video 96

---
+++ Backend/Video 96 - Installing Mongoose and using it in express js/main.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import express from 'express';
+import { Todo } from './models/Todo.js';
+
+const mainFunc = async ()=>{
+    var connection = await mongoose.connect("mongodb://localhost:27017/todo")
+}
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    const todo = new Todo({title: "Divyam", desc: "Buy summer clothes", isDone: false})
+    todo.save()
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
+mainFunc()
